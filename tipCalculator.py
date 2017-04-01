335463d062c803aab825d56a9adfde3d9afc3300
Tommy H. Yeargin, Jr <tommyyearginjr@users.noreply.github.com>
2017-04-01 10:55:57 -0400

    Update tipCalculator.py

--- tipCalculator.py
+++ tipCalculator.py
@@ -9,7 +9,3 @@ for i in range(0, 10001, 250):
     output = format((i/100), '.2f')
     tip = format(((i/100)*.15), '.2f')
     print ( "$" + str(output) + " ==========> " + "$" + str(tip) )
-
-
-
-
