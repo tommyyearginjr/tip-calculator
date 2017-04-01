de7d48bf0bf3e270ac44eaba9fece55b84a43e1d
Tommy H. Yeargin, Jr <tommy.yeargin@gmail.com>
2017-04-01 10:09:14 -0400

    tip calculator

--- tipCalculator.py
+++ tipCalculator.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3.5
 
-import os
-
 # Tip Calculator
-
-os.system('clear')
 
 title = "Tip Calculator"
 print(title + "\n" + (len(title))*"-" + "\n")
@@ -12,6 +8,8 @@ print(title + "\n" + (len(title))*"-" + "\n")
 for i in range(0, 10001, 250):
     output = format((i/100), '.2f')
     tip = format(((i/100)*.15), '.2f')
-    print (("${}".format((str(output)).zfill(2))) + " ==========> " + ("${}".format((str(tip)).zfill(2))))
+    print ( "$" + str(output) + " ==========> " + "$" + str(tip) )
+
+
 
 
