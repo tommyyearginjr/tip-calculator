a58b57760c7bdf14088bc517024ffdbcbe4969ff
Tommy H. Yeargin, Jr <tommyyearginjr@users.noreply.github.com>
2017-04-05 13:39:27 -0400

    $1 increments!

--- tipCalculator02.py
+++ tipCalculator02.py
@@ -1,6 +1,6 @@
 print(' TIP CALCULATOR'+'\n '+((len('TIP CALCULATOR'))*'-'))
 
-for i in range(0,101,2):
+for i in range(0,101,1):
 	t = str("{:.2f}".format(round((i*.15),2)))
 	i = str(i)
 	print("${} ===> ${}".format(i.zfill(3), t.zfill(5)))
