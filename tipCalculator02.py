e0c3c26d2875feb83729f8da18d5b47903d9fe1a
Tommy H. Yeargin, Jr <tommyyearginjr@users.noreply.github.com>
2017-04-05 11:41:59 -0400

    Create tipCalculator02.py

---
+++ tipCalculator02.py
@@ -0,0 +1,6 @@
+print(' TIP CALCULATOR'+'\n '+((len('TIP CALCULATOR'))*'-'))
+
+for i in range(0,101,2):
+	t = str("{:.2f}".format(round((i*.15),2)))
+	i = str(i)
+	print("${} ===> ${}".format(i.zfill(3), t.zfill(5)))
